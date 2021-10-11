1855973a03d3e430da0235c3a1da9f48da3f5846
Piloa <vincentGirard04@hotmail.fr>
2021-10-11 21:33:31 +0200

    index début texte

--- index.php
+++ index.php
@@ -6,6 +6,30 @@
 
 ?>
 
+<h1> PaintBallTraker's <h1>
+<section>
+
+<h2>Qu'est-ce que le paintball?</h2>
+<p> 
+    La paintball est une activité sportive ou de loisir opposant deux équipes dont les joueurs
+    sont équipés de masques de protection et de lanceur (ou marqueurs). Les lanceurs propulsent
+    par air comprimé au CO2 des billes de peinture de calibre 0.68 constituées de capsules médicales
+    renfermant une gélatine colorée, biodégradable.
+</p>
+
+</p>
+    Ces deux équipes  soppose l'une contre l'autre sur différents terrains délimités par des filets 
+    de protection.
+<p>
+
+<p> 
+    Le but est de réussir les différentes missions données par l'encadrant. La partie est gagnée lorque
+    l'objectif est atteint ou lorsque les joeurs d'une équipe sont totalement éliminés par l'équipe
+    adverses au moyen de leurs lanceurs.
+</p>
+
+</section>
+
 <?php
     include_once 'include/footeur.inc.php';
 ?>
