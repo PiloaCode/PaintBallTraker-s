ac58334dab40119c9460eae8772eddcb36852ab5
Alex6Mos <esalexismosquera@gmail.com>
2021-10-14 10:19:40 +0200

    Implémentation carte

--- index.php
+++ index.php
@@ -1,13 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
     <title>Document</title>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 </head>
 <body>
-    <h1>hi</h1>
-    <iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" sandbox="allow-forms allow-scripts allow-same-origin" src="https://www.geoportail.gouv.fr/embed/visu.html?c=4.102641455280456,46.7978436164658&z=6&l0=ORTHOIMAGERY.ORTHOPHOTOS::GEOPORTAIL:OGC:WMTS&permalink=yes" allowfullscreen></iframe>
+<h1>Implémentation d'une carte</h1>
+<div id="map">
+    <script>
+        var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('Coucou Vincent.<br> Easily customizable.')
+    .openPopup();
+    </script>
+
+</div>
+<!--
+<script type="text/javascript">
+    window.onload= function() {
+        var map = L.map("viewerDiv").setView([48.845,2.424],10) ;
+        L.tileLayer(
+            'https://wxs.ign.fr/53p4y6s38oqms2vkep7c0p0v/geoportail/wmts?service=WMTS&request=GetTile&version=1.0.0&tilematrixset=PM&tilematrix={z}&tilecol={x}&tilerow={y}&layer=ORTHOIMAGERY.ORTHOPHOTOS&format=image/jpeg&style=normal',
+            {
+                minZoom : 0,
+                maxZoom : 18,
+                tileSize : 256,
+                attribution : "IGN-F/Géoportail"
+            }).addTo(map);
+    }
+</script>
+-->
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,28 @@
+function go() {
+    var cartesLyr = new ol.layer.GeoportalWMTS({
+                layer: "GEOGRAPHICALGRIDSYSTEMS.MAPS",
+            }) ;
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            cartesLyr
+        ],
+        view: new ol.View({
+            center: [ 48.862725 , 2.287592],
+            zoom: 12
+        })
+    });    
+    var routeControl = new ol.control.Route({
+        collapsed : true
+    });
+    map.addControl(routeControl);
+}
+
+Gp.Services.getConfig({
+    apiKey: "53p4y6s38oqms2vkep7c0p0v",
+    onSuccess: go
+});
+/*
+var infoDiv = document.getElementById("info");
+infoDiv.innerHTML = "<p> Extension OL version " + Gp.olExtVersion + " (" + Gp.olExtDate + ")</p>";
+*/
