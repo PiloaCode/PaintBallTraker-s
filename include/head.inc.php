10a237052dca1cb049ebfaf9378b810b415524c0
Piloa <vincentGirard04@hotmail.fr>
2021-10-14 10:22:53 +0200

    head

--- include/head.inc.php
+++ include/head.inc.php
@@ -7,19 +7,22 @@
     
     <meta charset="utf-8"/>
     <meta name="author" content="Alexis Mosquera &amp; Vincent Girard" />
+    <link rel="stylesheet" href="style.css"     />
 
     <?php
         echo"<meta name=\"description\" content=\"$description\" />";
     ?>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li>Accueil</li>
-                <li>Recherche</li>
-                <li>Credit</li>
+    <header container-fluide>
+        <nav class="navFirst">
+            <ul class="navFirst">
+                <li> <a class="navFirst" href="index.php">Accueil </a></li>
+                <li> <a href= "" class="navFirst">Recherche </a></li>
+                <li><a class="navFirst" href="">Credit </a></li>
             </ul>
         </nav>
     </header>
