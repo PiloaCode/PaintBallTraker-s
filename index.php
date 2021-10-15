17226bd9ae9af2ba1d698588218a0fc4a1d2c361
Piloa <vincentGirard04@hotmail.fr>
2021-10-15 16:12:45 +0200

    index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 ?>
 
-<h1> PaintBallTraker's </h1>
+<h1 id="h1Paint"> PaintBallTraker's </h1>
 
 <section>
 
@@ -15,26 +15,65 @@
     La paintball est une activité sportive ou de loisir opposant deux équipes dont les joueurs
     sont équipés de masques de protection et de lanceur (ou marqueurs). Les lanceurs propulsent
     par air comprimé au CO2 des billes de peinture de calibre 0.68 constituées de capsules médicales
-    renfermant une gélatine colorée, biodégradable.
+    renfermant une gélatine colorée, biodégradable.Ces deux équipes  soppose l'une contre l'autre sur 
+    différents terrains délimités par des filets de protection. Le but est de réussir les différentes 
+    missions données par l'encadrant. La partie est gagnée lorque l'objectif est atteint ou lorsque 
+    les joeurs d'une équipe sont totalement éliminés par l'équipe adverses au moyen de leurs lanceurs.
 </p>
-
-<p class="intro">
-    Ces deux équipes  soppose l'une contre l'autre sur différents terrains délimités par des filets 
-    de protection.
-</p>
-
-<p class="intro"> 
-    Le but est de réussir les différentes missions données par l'encadrant. La partie est gagnée lorque
-    l'objectif est atteint ou lorsque les joeurs d'une équipe sont totalement éliminés par l'équipe
-    adverses au moyen de leurs lanceurs.
-</p>
-
-<table>
-    <td></td>
-    <td> </td>
-</table>
 
 </section>
+
+<div class='row'>
+
+<div class="col-12 col-lg-4">
+<div class="card maCard" id="card1">
+  <div class="imgCard">
+  <img class="card-img-top" src="include/icon-force.png" alt="Card image cap">
+  </div>
+  <div class="card-body">
+  <h5>Augmente la force et l'endurance</h5>
+    <p class="card-text"> Ramper, courir,plonger,esquiiver... Le paintball est un véritable concentré de plusieurs sports.
+      D'une manière générale, au sortir d'une partie, les joueurs sont réellement exténué.
+      En outre, cette activité tonifie le corps sans même que l'on s'en rende compte.</p>
+  </div>
+</div>
+</div>
+
+<div class="col-12 col-lg-4">
+<div class="card maCard" id="card2">
+<div id="imgCard2" class="imgCard">
+  <img class="card-img-top" src="include/icon-confiance.png" alt="Card image cap">
+</div>
+  <h5>Gain de confiance en soi</h5>
+  <div class="card-body">
+    <p class="card-text">Vous êtes plutôt timide dans la vie? Affirmez-vous lors des parties de paintball!
+      Souvent, cette activitépermet à des personnes plutôt réservées de prendre un leadership et 
+      d'accroître leur confiance en elle.
+
+    </p>
+  </div>
+</div>
+</div>
+
+<div class="col-12 col-lg-4">
+<div class="card maCard" id="card3" >
+  <div class="imgCard" id="imgCardCible">
+  <img class="card-img-top" src="include/icon-poids.png" alt="Card image cap">
+</div>
+  <h5> Permet la perte de poids </h5>
+  <div class="card-body">
+    <p class="card-text textCard">Si vous souhaitez perdre du poids, le paintball s'avère être une 
+      alternative tout à fait crédible et sérieuse. Du fait de l'exercice intense, vous
+      brûlez des calories et d'améliorez vos cycles de sommeil. Grâce à ce sport, vous
+      réduisez le risque de maladies cardiaques, pression artérielle et de dépression.
+      
+    </p>
+  </div>
+
+</div>
+</div>
+</div>
+
 
 <?php
     include_once 'include/footeur.inc.php';
