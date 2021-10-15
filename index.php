7a066f6e243b145e9b2ac8d6d174485c6d24b6ef
Alex6Mos <esalexismosquera@gmail.com>
2021-10-15 16:20:48 +0200

    MAJ 1;124

--- index.php
+++ index.php
@@ -10,6 +10,62 @@
       <section class="home-section">    
 
         <h2>Qu'est-ce que le paintball?</h2>
+
+        <!-------------CODE VINCENT------------->
+
+        <div class='row'>
+
+<div class="col-12 col-lg-4">
+<div class="card maCard" id="card1">
+  <div class="imgCard">
+  <img class="card-img-top" src="include/icon-force.png" alt="Card image cap">
+  </div>
+  <div class="card-body">
+  <h5>Augmente la force et l'endurance</h5>
+    <p class="card-text"> Ramper, courir,plonger,esquiiver... Le paintball est un véritable concentré de plusieurs sports.
+      D'une manière générale, au sortir d'une partie, les joueurs sont réellement exténué.
+      En outre, cette activité tonifie le corps sans même que l'on s'en rende compte.</p>
+  </div>
+</div>
+</div>
+
+<div class="col-12 col-lg-4">
+<div class="card maCard" id="card2">
+<div id="imgCard2" class="imgCard">
+  <img class="card-img-top" src="include/icon-confiance.png" alt="Card image cap">
+</div>
+  <h5>Gain de confiance en soi</h5>
+  <div class="card-body">
+    <p class="card-text">Vous êtes plutôt timide dans la vie? Affirmez-vous lors des parties de paintball!
+      Souvent, cette activitépermet à des personnes plutôt réservées de prendre un leadership et 
+      d'accroître leur confiance en elle.
+
+    </p>
+  </div>
+</div>
+</div>
+
+<div class="col-12 col-lg-4">
+<div class="card maCard" id="card3" >
+  <div class="imgCard" id="imgCardCible">
+  <img class="card-img-top" src="include/icon-poids.png" alt="Card image cap">
+</div>
+  <h5> Permet la perte de poids </h5>
+  <div class="card-body">
+    <p class="card-text textCard">Si vous souhaitez perdre du poids, le paintball s'avère être une 
+      alternative tout à fait crédible et sérieuse. Du fait de l'exercice intense, vous
+      brûlez des calories et d'améliorez vos cycles de sommeil. Grâce à ce sport, vous
+      réduisez le risque de maladies cardiaques, pression artérielle et de dépression.
+      
+    </p>
+  </div>
+
+</div>
+</div>
+</div>
+
+
+        <!-------------------------------------->
         <div>
           <p style="text-align: center;"> 
               La paintball est une activité sportive ou de loisir opposant deux équipes dont les joueurs
