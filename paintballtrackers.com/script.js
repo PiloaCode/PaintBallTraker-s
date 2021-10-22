6a40411f3fd5b93dcd357da0c774a47af0ce5e32
Alex6Mos <esalexismosquera@gmail.com>
2021-10-22 18:58:56 +0200

    1.129

--- paintballtrackers.com/script.js
+++ paintballtrackers.com/script.js
@@ -2,6 +2,6 @@ $(function() {
     "use strict";
     $(".pCard_add").click(function() {
         $(".pCard_card").toggleClass("pCard_on");
-        $(".pCard_add i").toggleClass("fa-minus");
+        $(".pCard_add i").toggleClass("fas fa-minus");
     });
 });
